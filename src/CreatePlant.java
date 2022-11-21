74da6ddba239467d0808d7f75ded3b44fe0a36a8
Stefan Franzén <stefra90@live.se>
2022-11-21 23:44:43 +0100

    Added enum CreatePlant

---
+++ src/CreatePlant.java
@@ -0,0 +1,13 @@
+public enum CreatePlant {
+    IGGE ("Igge"),
+    LAURA ("Laura"),
+    PUTTE ("Putte"),
+    MEATLOAF ("Meatloaf");
+
+    public final String plantName;
+
+    CreatePlant(String p) {
+        plantName = p;
+    }
+}
+
